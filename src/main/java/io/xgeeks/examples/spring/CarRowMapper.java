5ade47af212c788efbc634848b2a4debc233643f
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-18 20:52:03 +0000

    creates rowmapper to car

--- src/main/java/io/xgeeks/examples/spring/CarRowMapper.java
+++ src/main/java/io/xgeeks/examples/spring/CarRowMapper.java
@@ -1,2 +1,22 @@
-package io.xgeeks.examples.spring;public class CarRowMapper {
+package io.xgeeks.examples.spring;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CarRowMapper implements RowMapper<Car> {
+
+    @Override
+    public Car mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        Long id = resultSet.getLong("ID");
+        String name = resultSet.getString("name");
+        String city = resultSet.getString("city");
+        String model = resultSet.getString("model");
+        String color = resultSet.getString("color");
+        return Car.builder().withId(id).withName(name)
+                .withCity(city)
+                .withModel(model)
+                .withColor(color).build();
+    }
 }
