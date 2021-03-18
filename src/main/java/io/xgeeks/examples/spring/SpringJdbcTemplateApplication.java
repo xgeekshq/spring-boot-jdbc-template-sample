b28aadb28e15e412e0df0348c95afcf351bb3e2f
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-18 19:46:24 +0000

    initial project

---
+++ src/main/java/io/xgeeks/examples/spring/SpringJdbcTemplateApplication.java
@@ -0,0 +1,13 @@
+package io.xgeeks.examples.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/xgeeks/examples/spring/SpringJdbcTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.xgeeks.examples.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
