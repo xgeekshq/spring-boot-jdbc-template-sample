e56f73fd39b8ac0368506231934a53022e13d612
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-19 14:29:20 +0000

    creates helloworld controller

--- src/main/java/io/xgeeks/examples/spring/HelloWorldController.java
+++ src/main/java/io/xgeeks/examples/spring/HelloWorldController.java
@@ -1,2 +1,16 @@
-package io.xgeeks.examples.spring;public class HelloWorldController {
+package io.xgeeks.examples.spring;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+
+    @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
+    public String hello() {
+        return "Hello World Spring Boot using Platform.sh";
+    }
 }
