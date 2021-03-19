cf1a9bad7e46399b555892b9ab22d43bf238330e
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-19 14:29:06 +0000

    creates CarDao

--- src/test/java/io/xgeeks/examples/spring/CarDAOTest.java
+++ src/test/java/io/xgeeks/examples/spring/CarDAOTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 @ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class CarDAOTest {
 
     @Autowired
@@ -15,5 +19,7 @@ class CarDAOTest {
     @Test
     public void shouldBeNotNull() {
         Assertions.assertNotNull(carDAO);
+        Optional<Car> by = carDAO.findBy(1L);
+        Assertions.assertNotNull(by);
     }
 }
