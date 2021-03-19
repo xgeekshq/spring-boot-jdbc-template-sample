f3e1a72a3e7e5841601c2c53cf2bc136e21f1df9
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-19 16:03:23 +0000

    creates page

--- src/main/java/io/xgeeks/examples/spring/Page.java
+++ src/main/java/io/xgeeks/examples/spring/Page.java
@@ -1,2 +1,30 @@
-package io.xgeeks.examples.spring;public class Page {
+package io.xgeeks.examples.spring;
+
+public final class Page {
+
+    private final long page;
+
+    private final long size;
+
+    public Page(long page, long size) {
+        this.page = page;
+        this.size = size;
+    }
+
+
+    public long getLimit() {
+        return size;
+    }
+
+    public long getOffset() {
+        return (page - 1) * size;
+    }
+
+    @Override
+    public String toString() {
+        return "Page{" +
+                "page=" + page +
+                ", size=" + size +
+                '}';
+    }
 }
