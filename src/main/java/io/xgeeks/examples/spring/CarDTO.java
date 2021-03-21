f9379224242a0debb1fb7aaa6e914f8a78f37ce2
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-21 17:57:39 +0000

    creates getter and setter in the cardto

--- src/main/java/io/xgeeks/examples/spring/CarDTO.java
+++ src/main/java/io/xgeeks/examples/spring/CarDTO.java
@@ -1,4 +1,56 @@
 package io.xgeeks.examples.spring;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 public class CarDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String city;
+
+    private String model;
+
+    private String color;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
