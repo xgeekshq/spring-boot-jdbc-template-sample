f0233b79715aec74ff727e1d01d1ac50b0d05a1f
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-21 18:31:14 +0000

    creates entity not found exception

---
+++ src/main/java/io/xgeeks/examples/spring/EntityNotFoundException.java
@@ -0,0 +1,11 @@
+package io.xgeeks.examples.spring;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
