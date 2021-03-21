43f320373fcb1c01ca1fb4a6f78cd44d87470906
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-21 18:44:03 +0000

    creates insert dto test

--- src/test/java/io/xgeeks/examples/spring/CarControllerTest.java
+++ src/test/java/io/xgeeks/examples/spring/CarControllerTest.java
@@ -1,10 +1,13 @@
 package io.xgeeks.examples.spring;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CarControllerTest {
@@ -17,7 +20,19 @@ class CarControllerTest {
 
     @Test
     public void shouldInsert() {
+        CarDTO carDTO = new CarDTO();
+        carDTO.setCity("Salvador");
+        carDTO.setColor("Blue");
+        carDTO.setModel("Model");
+        carDTO.setName("Car");
+        CarDTO dto = this.template.postForObject("http://localhost:" + port + "/cars", carDTO, CarDTO.class);
 
+        Assertions.assertNotNull(dto);
+        Assertions.assertNotNull(dto.getId());
+        Assertions.assertEquals(carDTO.getCity(), dto.getCity());
+        Assertions.assertEquals(carDTO.getColor(), dto.getColor());
+        Assertions.assertEquals(carDTO.getModel(), dto.getModel());
+        Assertions.assertEquals(carDTO.getName(), dto.getName());
     }
 
     public void shouldFindById() {}
