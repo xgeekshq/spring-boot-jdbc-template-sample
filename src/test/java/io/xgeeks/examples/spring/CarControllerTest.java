772a875de681a3c7e3dec9b92cc81ff65667a042
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-21 18:40:40 +0000

    creates create to car controller

--- src/test/java/io/xgeeks/examples/spring/CarControllerTest.java
+++ src/test/java/io/xgeeks/examples/spring/CarControllerTest.java
@@ -6,10 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class HelloWorldControllerTest {
+class CarControllerTest {
 
     @Autowired
     private TestRestTemplate template;
@@ -18,8 +16,15 @@ class HelloWorldControllerTest {
     private int port;
 
     @Test
-    public void shouldReturnHelloWorld() {
-        assertThat(this.template.getForObject("http://localhost:" + port + "/",
-                String.class)).contains("Hello World Spring Boot");
+    public void shouldInsert() {
+
     }
+
+    public void shouldFindById() {}
+    public void shouldReturnNotFound(){}
+    public void shouldUpdate() {}
+    public void shouldRemove() {}
+    public void shouldFindAll(){}
+
+
 }
