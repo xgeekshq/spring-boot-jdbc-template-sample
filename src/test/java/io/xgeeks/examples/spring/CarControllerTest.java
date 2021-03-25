3b98d2ff24a08ddf228ccc0debfae8cd6d3f8ff6
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-25 16:30:13 +0000

    fixes imports in the CarControllerTest

--- src/test/java/io/xgeeks/examples/spring/CarControllerTest.java
+++ src/test/java/io/xgeeks/examples/spring/CarControllerTest.java
@@ -1,7 +1,5 @@
 package io.xgeeks.examples.spring;
 
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
